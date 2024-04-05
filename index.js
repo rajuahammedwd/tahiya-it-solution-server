32942b2c40c73325e39500b42755c762d8606687
rajuahammedwd <raju720190r@gmail.com>
2024-04-05 17:27:31 +0545

    server side deploy

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Tahiya IT Solution Server is running')
+})
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tdoicmr.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+  
+    const productCollection = client.db("tahiyaItSolution").collection("products");
+     
+    app.get('/products', async(req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    })
+    app.post('/products', async(req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Tahiya IT Solution Server is running on port ${port}`)
+})
